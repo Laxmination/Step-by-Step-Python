35f54ec22cbae9f3636bf80f4955611b809b21d1
laxmination <laxmikathayat52@gmail.com>
2024-02-29 20:16:40 +0545

    orfvds

--- day4.py
+++ day4.py
@@ -2,3 +2,5 @@ name = input("What is your name")
 age = input("What is your age?")
 pergentage = input("What is your percentage marks in this result?")
 print("My name is", name, ". I am ", age, " years old and the total percentage I obtained from this exam is ", pergentage)
+
+
