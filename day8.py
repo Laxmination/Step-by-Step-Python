3d65e1931f5a9874317a11de3d4457a1a9b43373
laxmination <laxmikathayat52@gmail.com>
2024-03-07 23:26:59 +0545

    list example

---
+++ day8.py
@@ -0,0 +1,37 @@
+# WAP to ask the user to enter names of their 3 favorite movies & store them in a list.
+list1 =[]
+movie1= (input("Enter first fav. movie Name: "))
+movie2= (input("Enter second fav. movie Name: "))
+movie3= (input("Enter third fav. movie Name: "))
+list1.append(movie1)
+list1.append(movie2)
+list1.append(movie3)
+print(list1)
+
+#OR
+
+# list1 =[]
+# movie= (input("Enter first fav. movie Name: "))
+# list1.append(movie)
+# movie= (input("Enter second fav. movie Name: "))
+# list1.append(movie)
+# movie= (input("Enter third fav. movie Name: "))
+# list1.append(movie)
+# print(list1)
+
+
+#OR
+
+# list1 =[]
+# list1.append(input("Enter first fav. movie Name: "))
+# list1.append(input("Enter second fav. movie Name: "))
+# list1.append(input("Enter third fav. movie Name: "))
+# print(list1)
+
+#OR
+
+# movie1= (input("Enter first fav. movie Name: "))
+# movie2= (input("Enter second fav. movie Name: "))
+# movie3= (input("Enter third fav. movie Name: "))
+# list1 =list[movie1, movie2, movie3]
+# print(list1)
