82010d2ad7cd390cbf3d29a0a0db769c843217ef
laxmination <laxmikathayat52@gmail.com>
2024-03-08 00:13:31 +0545

    tuple example

---
+++ day8_3.py
@@ -0,0 +1,5 @@
+#WAP to count the number of the students with the "A" grade in the following tuple
+marks =("B", "A", "C", "B", "A", "D", "A", "E")
+print(marks)
+print(marks.count("A"))
+print(marks.index("E"))
