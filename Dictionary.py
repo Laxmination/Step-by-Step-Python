425e86652dd35de2ea46a3073c5c5fba5b8def50
laxmination <laxmikathayat52@gmail.com>
2024-03-11 21:36:25 +0545

    example of Dictionary

---
+++ Dictionary.py
@@ -0,0 +1,9 @@
+# Storing some word meanings in the python dictionary
+
+Dict1 = {
+    "Cat": "A small animal",
+    "Table": ["A piece of furniture", "List of facts and figures"]
+
+}
+
+print(Dict1)
