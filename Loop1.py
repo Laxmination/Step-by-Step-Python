b700c9b15ebdfa471c6303e99e71f9ad74c093de
laxmination <laxmikathayat52@gmail.com>
2024-03-15 13:55:20 +0545

    1000 to 1

--- Loop1.py
+++ Loop1.py
@@ -2,5 +2,5 @@ num =1
 while num <=1000 :
     print(num)
     num += 1
-print("Count ended")
 
+print("Count ended")
---
+++ loop2.py
@@ -0,0 +1,6 @@
+numb =1000
+while numb >=1:
+    print(numb)
+    numb -=1
+
+print("Loop ended")
