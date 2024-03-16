bab52d51d8b91e269290cde533f94a05fc73aa50
laxmination <laxmikathayat52@gmail.com>
2024-03-16 13:03:36 +0545

    Program for searching number in tuple using loop

---
+++ loop8.py
@@ -0,0 +1,10 @@
+tupl =(1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 49, 169, 196, 225, 256, 49, 400, 49)
+ 
+x =49
+indx =0
+for num in tupl:
+    if (num ==x):
+       print("Number is at index",indx )
+    else:
+        print("Loading...")
+    indx +=1
