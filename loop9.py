837fb4c44fad11de0c2e8ad8feb6d33f31919e8b
laxmination <laxmikathayat52@gmail.com>
2024-03-16 13:46:20 +0545

    sum of first n numbers using loop and range

--- loop9.py
+++ loop9.py
@@ -1,3 +1,14 @@
+# n = int(input("Enter any number: "))
+# sum1 =0
+
+# i =1
+# while i <= n:
+#     sum1 += i
+#     i +=1
+
+# print("Total factorial =", sum1)
+
+
 n =range(int(input("Enter range of numbers: ")))
 
 sum1 =0
