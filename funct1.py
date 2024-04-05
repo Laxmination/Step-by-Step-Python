ac4c55762d93c8ea040c371a1cefd6c0232d435f
laxmination <laxmikathayat52@gmail.com>
2024-04-05 06:52:27 +0545

    Function to print length of list

---
+++ funct1.py
@@ -0,0 +1,9 @@
+cities =["Kathmandu", "Pokhara", "Birgunj", "Dhangadi", "Dharan", "Biratnagar"]
+heros =["Rajesh Hamal", "Nikhil Upreti", "Biraj Bhatta", "Dahyang Rai"]
+
+def print_len(list):
+    print(len(list))
+
+
+print_len(cities)
+print_len(heros)
