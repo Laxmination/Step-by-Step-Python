2ca45d0e9eb5ef7be3ab4df2f6e6ba9d9a3ea59c
laxmination <laxmikathayat52@gmail.com>
2024-04-07 06:58:45 +0545

    comment added

--- funct1.py
+++ funct1.py
@@ -1,9 +1,9 @@
 cities =["Kathmandu", "Pokhara", "Birgunj", "Dhangadi", "Dharan", "Biratnagar"]
 heros =["Rajesh Hamal", "Nikhil Upreti", "Biraj Bhatta", "Dahyang Rai"]
 
-def print_len(list):
+def print_len(list): #function definition
     print(len(list))
 
 
-print_len(cities)
+print_len(cities) #function call
 print_len(heros)
