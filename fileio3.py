27d490a6da74c4e8f8ecd7fa9f670d3ff79859e9
laxmination <laxmikathayat52@gmail.com>
2024-04-10 10:39:37 +0545

    program to count even numbers in a txt file

---
+++ fileio3.py
@@ -0,0 +1,22 @@
+# to count even numbers from the file containing numbers seperated by comma
+
+count =0
+with open("sample2.txt", "r") as f:
+    data =f.read()
+  
+    # numb =""
+    # for i in range(len(data)):
+    #     if (data[i] == ","):
+    #         print(int(numb))
+    #         numb =""
+    #     else:
+    #         numb +=data[i]
+
+
+
+    numb = data.split(",")
+    for val in numb:
+        if(int(val) %2 ==0):
+            count +=1
+
+print(count)
