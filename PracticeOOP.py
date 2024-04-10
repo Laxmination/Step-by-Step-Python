f921b8cb9cb109cf394ec61421700306281aa590
laxmination <laxmikathayat52@gmail.com>
2024-04-10 14:18:01 +0545

    comment added

--- PracticeOOP.py
+++ PracticeOOP.py
@@ -1,16 +1,16 @@
-class Book:
+class Book: #Book is class here
     name = "Harry Potter"
     writer = "J.K Rowling"
 
-book1 = Book()
-print(book1.name)
-print(book1.writer)
+book1 = Book() #book1 is object here
+print(book1.name) # name is attribute here
+print(book1.writer) # writer is an attribute here 
 
 
-class X:
+class X: # X is class here
     value = 10
     position = "x-axis"
 
-x1 = X()
-print(x1.value)
+x1 = X() # x1 is object here 
+print(x1.value) #value is an attribute here 
 print(x1.position)
