286392cc1d6f81b1bdc330e7fe45479757044438
laxmination <laxmikathayat52@gmail.com>
2024-04-10 14:10:22 +0545

    random practice of oop

---
+++ PracticeOOP.py
@@ -0,0 +1,16 @@
+class Book:
+    name = "Harry Potter"
+    writer = "J.K Rowling"
+
+book1 = Book()
+print(book1.name)
+print(book1.writer)
+
+
+class X:
+    value = 10
+    position = "x-axis"
+
+x1 = X()
+print(x1.value)
+print(x1.position)
