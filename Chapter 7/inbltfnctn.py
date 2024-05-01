4e67ced91d3060ecd808eb0e0fcb75b6690dc26e
laxmination <laxmikathayat52@gmail.com>
2024-05-01 12:17:05 +0545

    some math functions example

---
+++ Chapter 7/inbltfnctn.py
@@ -0,0 +1,12 @@
+import math
+
+pi = 3.14
+x, y, z = 2, 3, 4
+print(round(pi)) # This function will roundup the decimal value
+print(math.ceil(pi)) # This function will roundup to upper integer value
+print(math.floor(pi)) # this function will roundup to the lower integer value
+print(abs(pi)) # this function will display the absolute value
+print(pow(pi, 3)) # this function displays the value of pi to the power 3
+print(math.sqrt(49)) # this function displays square root of 49
+print(max(x, y, z)) # this function displays maximum value among these three given data
+print(min(x, y, z)) # this fucntion will displays minimum value among these
