3a3685b8771e0b43937668d45eca4169b3878711
laxmination <laxmikathayat52@gmail.com>
2024-05-01 12:48:42 +0545

    slicing example

--- Chapter 3/StrCount.py
+++ Chapter 3/StrCount.py
@@ -11,3 +11,14 @@ print(B.upper()) # all letters will be in upper case
 print(A.lower()) # all letters will be in lower case
 print(B.replace("t", "p")) # It will replaces t with p
 print(B[0:4]) # This function will display the string character from 0 to 3
+print(B[0:7:3]) # It will display the string character third after previous character [Start:stop:step]
+print(B[::2]) # It will display the string character second after the previous character
+print(B[::-1]) # It reverses the string and display
+
+website = "http://google.com"
+website2 ="http://twitter.com"
+
+slice = slice(7, -4) # slices the string and return the sliced piece
+
+print(website[slice]) 
+print(website2[slice])
