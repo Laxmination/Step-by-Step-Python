b76782bf9db59e11ddac5aaf59bc53082eef5290
laxmination <laxmikathayat52@gmail.com>
2024-05-01 11:54:07 +0545

    some string methods example

--- Chapter 3/StrCount.py
+++ Chapter 3/StrCount.py
@@ -1,4 +1,12 @@
 #WAP to find the occurrence of 's' in String.
-B ="String"
+B ="string"
+A ="STrInG"
 print(B)
-print(B.count("S"))
+print(B.count("s"))
+print(B *3) # it will print string 3 times
+print(B.isalpha()) # It gives true 
+print(B.isdigit()) # It gives false
+print(B.capitalize()) # the first letter will be capital
+print(B.upper()) # all letters will be in upper case
+print(A.lower()) # all letters will be in lower case
+print(B.replace("t", "p")) # It will replaces t with p
