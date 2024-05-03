b8fcf50dcec1fc441eff76692236a666f56f329b
laxmination <laxmikathayat52@gmail.com>
2024-05-03 21:31:50 +0545

    a simple continue example

--- Chapter 6/loop.py
+++ Chapter 6/loop.py
@@ -1,4 +1,11 @@
-while True:
-    name = input("What is your Name: ")
-    if name != "":
-        break
+# while True:
+#     name = input("What is your Name: ")
+#     if name != "":
+#         break
+
+phoneNumber = "+977-983-456-5678"
+
+for i in phoneNumber:
+    if i == "-":
+        continue
+    print(i, end="")
