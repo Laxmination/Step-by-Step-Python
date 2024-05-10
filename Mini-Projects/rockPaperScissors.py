aee5a6686f208ba9622e6497f34ddfe2994c26bd
laxmination <laxmikathayat52@gmail.com>
2024-05-10 22:32:47 +0545

    some improvement in rock paper scissor game

--- Mini-Projects/rockPaperScissors.py
+++ Mini-Projects/rockPaperScissors.py
@@ -1,24 +1,33 @@
 import random
 
-choice = ["rock", "paper", "scissor"]
+while True:
+    choice = ["rock", "paper", "scissor"]
 
-computer = random.choice(choice)
-player = None
+    computer = random.choice(choice)
+    player = None
 
-while player not in choice:
-    player = input("rock, paper or scissors?: ")
+    while player not in choice:
+        player = input("rock, paper or scissors?: ")
 
-if player == computer:
-    print("Computer: ", computer)
-    print("Player: ", player)
-    print("Tie!")
+    if player == computer:
+        print("Computer: ", computer)
+        print("Player: ", player)
+        print("Tie!")
 
-elif player == "paper" and computer == "scissors" or player =="rock" and computer =="paper" or player =="scissor" and computer =="paper":
-    print("Computer: ", computer)
-    print("Player: ", player)
-    print("Oooopppss:(")
+    elif player == "paper" and computer == "scissors" or player =="rock" and computer =="paper" or player =="scissor" and computer =="paper":
+        print("Computer: ", computer)
+        print("Player: ", player)
+        print("Oooopppss:(")
 
-elif player =="scissor" and computer =="paper" or player =="paper" and computer =="rock" or player =="rock" and computer =="scissor":
-    print("Computer: ", computer)
-    print("Player: ", player)
-    print("Kangrachulason!")
+    elif player =="scissor" and computer =="paper" or player =="paper" and computer =="rock" or player =="rock" and computer =="scissor":
+        print("Computer: ", computer)
+        print("Player: ", player)
+        print("Kangrachulason!")
+
+
+    play_again = input("Play again? (yes/no): ")
+
+    if play_again != "yes":
+        break
+
+print("Well Played!!!")
