4e49db77f48b48642854316bdd0b8b4033009804
laxmination <laxmikathayat52@gmail.com>
2024-05-10 16:03:00 +0545

    exception handellig example

---
+++ Chapter 10/example2.py
@@ -0,0 +1,8 @@
+try:
+    a = int(input("Enter a number: "))
+    b = int(input("Enter anpther number: "))
+    result = a /b
+    print(result)
+
+except Exception:
+    print("Something went wrong:(")
