2fe9681381b6fb1074b3dea7a070b96a0ed85bb3
laxmination <laxmikathayat52@gmail.com>
2024-05-10 16:25:47 +0545

    exception handelling example improved

--- Chapter 10/example2.py
+++ Chapter 10/example2.py
@@ -2,7 +2,15 @@ try:
     a = int(input("Enter a number: "))
     b = int(input("Enter anpther number: "))
     result = a /b
-    print(result)
 
+except ZeroDivisionError as e:
+    print("Zero le divide garna sakdainau! murakh!")
+    print(e)
+except ValueError as v:
+    print("Enter only numbers idiot!")
+    print(v)
 except Exception:
     print("Something went wrong:(")
+
+else:
+    print(result)
